a60d22c7e370792d9df4505834e49cf5417b1d8e
PavelZabalotny <pavelzabalotny@gmail.com>
2022-02-13 01:06:49 +0300

    fix: adaptive and styles

--- movie-app/js/index.js
+++ movie-app/js/index.js
@@ -1,8 +1,7 @@
 import customFetch from "./fetch.js"
 
-// const API_KEY = 'd952a794'
 let searchValue = 'matrix'
-// const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}`
+
 const movieCards = document.querySelector('.movie-cards')
 const search = document.querySelector('.search')
 const headerSearch = document.querySelector('.header__search')
@@ -15,7 +14,6 @@ function getUrl (searchValue) {
 
 async function getMovies (url, targetContainer) {
   const json = await customFetch(url)
-  // console.log(json)
   const movies = json?.map(({Title, Year, Type, Poster}) => (
     `<div class="movie-card">
       <img src="${Poster}" alt="${Title}">
@@ -26,7 +24,7 @@ async function getMovies (url, targetContainer) {
       </div>      
     </div>`))
 
-  movieCards.innerHTML = movies?.join('') || `<div class="nothing">Nothing founds</div>`
+  targetContainer.innerHTML = movies?.join('') || `<div class="nothing">Nothing founds</div>`
 }
 
 await getMovies(getUrl(searchValue), movieCards)
@@ -36,17 +34,12 @@ headerSearch.addEventListener('submit', submitHandler)
 deleteButton.addEventListener('click', deleteButtonHandler)
 
 function changeHandler(e) {
-  // console.log(e.target.value)
   searchValue = e.target.value
   deleteButton.style.display = e.target.value ? 'block' : 'none'
-  // const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}`
-  // await getMovies(getUrl(searchValue), movieCards)
 }
 
 async function submitHandler(e) {
   e.preventDefault()
-  // console.log(searchValue)
-  // const url = `https://www.omdbapi.com/?apikey=${API_KEY}&s=${searchValue}`
   await getMovies(getUrl(searchValue), movieCards)
   search.value = searchValue
 }
