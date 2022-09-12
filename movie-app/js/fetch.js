8ee931b39057b7deb01bc66921db586ba997a014
Pavel Zabalotny <pavelzabalotny@gmail.com>
2022-09-12 23:39:51 +0300

    refactor: delete movie-app

--- movie-app/js/fetch.js
+++
@@ -1,9 +0,0 @@
-export default async function (url) {
-  try {
-    const response = await fetch(url)
-    const json = await response.json()
-    return json.Search
-  } catch (e) {
-    console.log(e.message)
-  }
-}
